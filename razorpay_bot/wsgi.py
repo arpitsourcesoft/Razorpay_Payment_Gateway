ea6ae937909881fa6249010c1941db19101e2e7c
arpitsourcesoft <arpit@sourcesoftsolutions.com>
2022-07-20 14:09:30 +0530

    Setting AND WSGI CHANGES

--- razorpay_bot/wsgi.py
+++ razorpay_bot/wsgi.py
@@ -7,11 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+# from dj_static import Cling
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'razorpay_bot.settings')
+
+# application = Cling(get_wsgi_application())
+
 import os
-
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'razorpay_bot.settings')
+from whitenoise import WhiteNoise
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_obs.settings')
